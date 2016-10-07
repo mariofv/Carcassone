67632c9a0396beea368a36abd7eba8fd85db2fb3
Daryan7 <mario_bcn2@hotmail.es>
2016-10-08 00:07:40 +0200

    asassd

--- GameManager.cs
+++ GameManager.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
 	LosetaAtributos[,] board = new LosetaAtributos[72,72];
 
+	Jugador[] jugadores = new Jugador[4];
 
+	Stack<GameObject> losetasAPoner; 
 
 	void Start() {
-		
+		for (int i = 0; i < jugadores.Length; ++i) {
+			Jugador jugador = new Jugador();
+			jugador.puntos = 0;
+			jugadores [i] = jugador;
+		}
+
 	}
 
 	void Update () {
