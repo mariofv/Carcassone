0bea09ad217f8b350924cb616f64f820b5d83d7f
Mario <Mario>
2016-10-08 01:10:29 +0200

    DDD

--- Scripts/Loseta.cs
+++ Scripts/Loseta.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class Loseta : MonoBehaviour {
 
-	//Este vector tiene los tipos de cada lado de la ficha
+	//Estos vectores tiene los tipos de cada lado de la ficha
 	public int[] ladosLoseta;
 	public tipoLoseta[] tiposEnLoseta;
 	//Donde esta situado el subdito, -1 indica sin subdito.
 	public tipoLoseta tipoSubdito;
+	//El subdito :D
 	Subdito subdito;
 
 	// Use this for initialization
