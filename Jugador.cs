735c00c7658758616fd5277364105cad790506c7
joan <joansalto14@gmail.com>
2016-10-08 00:41:06 +0200

    jugador.cs

---
+++ Jugador.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class Jugador : MonoBehaviour {
+	
+	public int puntos;
+	public int subditos;
+	
+	// Use this for initialization
+	void Start () {
+		puntos = 0;
+		subditos = 8;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	//Si tienes subdito disponible lo resta, sino avisa de ERROR
+	bool ColocaSubdito() {
+		if(subditos > 0) --subditos;
+		else print("No tienes subditos");
+	}
+}
