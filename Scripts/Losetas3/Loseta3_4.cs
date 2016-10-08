3d43c19b77366dd81e7515ad2e792966eed8eb22
Mario <Mario>
2016-10-08 02:40:34 +0200

    HOLAA:AAD:D:D:D:ASD:

---
+++ Scripts/Losetas3/Loseta3_4.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loseta3_4 :  Loseta {
+
+	// Use this for initialization
+	void Start () {
+		tiposEnLoseta = new tipoLoseta[] {tipoLoseta.CAMINO,tipoLoseta.CIUDAD,tipoLoseta.CAMPO};
+		ladosLoseta = new int[] { 0, 1, 3, 0, 0 };
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Scripts/Losetas3/Loseta3_5.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loseta3_5 : Loseta {
+
+	// Use this for initialization
+	void Start () {
+		tiposEnLoseta = new tipoLoseta[] {tipoLoseta.CAMINO,tipoLoseta.CIUDAD,tipoLoseta.CAMPO};
+		ladosLoseta = new int[] { 1,0,0,3,1 };
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
