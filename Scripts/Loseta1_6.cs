ca030436b5c7d1d3ce76746d0a93c2b39e0778f4
EnderRL <carlosroldanmontaner@gmail.com>
2016-10-08 03:00:17 +0200

    Loseta1

---
+++ Scripts/Loseta1_6.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loseta1_6 : Loseta {
+
+	// Use this for initialization
+	void Start () {
+		tiposEnLoseta = new tipoLoseta[] {tipoLoseta.CIUDAD,tipoLoseta.CAMINO};
+		ladosLoseta = new int[] { 0, 0, 1, 0, 0 };
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Scripts/Loseta1_9.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loseta1_9 : Loseta {
+
+	// Use this for initialization
+	void Start () {
+		tiposEnLoseta = new tipoLoseta[] {tipoLoseta.CAMPO,tipoLoseta.CAMINO,tipoLoseta.CAMINO,tipoLoseta.CAMINO};
+		ladosLoseta = new int[] { 0, 1, 2, 3, 0 };
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
