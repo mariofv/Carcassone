3137c5ae26c52334fa213cd76cccf09d4999c71c
Mario <Mario>
2016-10-08 03:07:09 +0200

    HOLAgit add -A

---
+++ Scripts/Losetas3/Loseta3_9.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loseta3_9 :  Loseta {
+
+	// Use this for initialization
+	void Start () {
+		tiposEnLoseta = new tipoLoseta[] {tipoLoseta.CAMPO,tipoLoseta.CAMINO};
+		ladosLoseta = new int[] {0,0,1,1,1};
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
