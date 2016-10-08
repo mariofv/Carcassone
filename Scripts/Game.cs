79cf61145c9d845d4a0a8f0da4611228a5ad0cce
Daryan7 <mario_bcn2@hotmail.es>
2016-10-08 02:35:41 +0200

    asdas

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -4,9 +4,17 @@ using System.Collections.Generic;
 
 public class Game : MonoBehaviour {
 
+	struct Coord {
+		public int x, y;
+	};
+
 	Loseta[,] board = new Loseta[74, 74];
 
 	Jugador[] jugadores = new Jugador[4];
+
+	LinkedList<Coord> posiblesLosetas;
+
+	Stack<GameObject> losetasAColocar;
 
 	const int numF = 10;
 
@@ -20,6 +28,7 @@ public class Game : MonoBehaviour {
 		for (int i = 0; i < numF; ++i) {
 			prueba.Add(i);
 		}
+		losetasAColocar = new Stack<GameObject> ();
 		while (prueba.Count > 0) {
 			int rand = Random.Range (0, prueba.Count);
 			int selected = prueba [rand];
@@ -27,7 +36,26 @@ public class Game : MonoBehaviour {
 		}
 	}
 
+	int possibleMovement(Loseta origen, Loseta adyacente) {
+		return 0;
+	}
+
+	void place(GameObject loseta, int x, int y) {
+	}
+
+	int[] sumX = {0, 1, 0, -1};
+	int[] sumY = { 1, 0, -1, 0 };
+
 	void Update () {
-	
+		for (int i = 0; i < jugadores.Length; ++i) {
+			Jugador jugador = jugadores [i];
+			GameObject loseta = losetasAColocar.Pop ();
+			foreach (Coord pos in posiblesLosetas) {
+				int dir;
+				if ((dir = possibleMovement (loseta.GetComponent<Loseta> (), board [pos.x, pos.y])) != -1) {
+					place (loseta, pos.x + sumX[dir], pos.y + sumY[dir]);
+				}
+			}
+		}
 	}
 }
