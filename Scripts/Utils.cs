3e7fef1e3f3933525160714956810dbcf8ab73a4
Daryan7 <mario_bcn2@hotmail.es>
2016-10-08 17:44:49 +0200

    asada

--- Scripts/Utils.cs
+++ Scripts/Utils.cs
@@ -18,6 +18,10 @@ public class Utils : MonoBehaviour {
 		return -x;
 	}
 
+	public static int opuesto(int dir) {
+		return (dir + 2) % 4;
+	}
+
 	public static int grau (int i, int j) {
 		if(i > j) return (i-j)*90;
 		else if (i < j) return (j-i)*(-90);
